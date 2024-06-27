67fe1796cdb74c16b39460da019e2c64e40ebde4
imranaakhtar <imrana.akhtar@callawaygolf.com>
2024-06-27 21:20:17 +0530

    router

--- src/Components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Header() {
-  return (
-    <>
-        <p className='header'>This is e-commerce site. </p>
-    </>
-  )
-}
--- src/Pages/homepage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function homepage() {
-  return (
-    <p className='row p-5'>
-      Best Sellers in the market
-    </p>
-  )
-}
